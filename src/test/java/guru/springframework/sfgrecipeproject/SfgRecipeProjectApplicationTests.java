b63a057ebf67095522095b5f691c5e2b8d1fb29c
zosoyi <cjsz321qq@gmail.com>
2022-01-27 20:28:36 +0900

    spring initialize

---
+++ src/test/java/guru/springframework/sfgrecipeproject/SfgRecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgrecipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRecipeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
