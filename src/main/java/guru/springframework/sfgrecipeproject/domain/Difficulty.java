d51de4d0ce4e5d8a7c6f8af695d731172fc17418
zosoyi <cjsz321qq@gmail.com>
2022-02-04 17:42:29 +0900

    Add Enum of Difficulty, closes #5

---
+++ src/main/java/guru/springframework/sfgrecipeproject/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package guru.springframework.sfgrecipeproject.domain;
+
+public enum Difficulty {
+    EASY, MODERATE, HARD
+}
--- src/main/java/guru/springframework/sfgrecipeproject/domain/Recipe.java
+++ src/main/java/guru/springframework/sfgrecipeproject/domain/Recipe.java
@@ -27,6 +27,9 @@ public class Recipe {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     public Long getId() {
         return id;
     }
@@ -113,5 +116,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
