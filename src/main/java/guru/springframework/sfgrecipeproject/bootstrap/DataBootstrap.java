c5039ad68b4375d32874a5fbbe3a8481dace7445
zosoyi <cjsz321qq@gmail.com>
2022-02-08 21:58:56 +0900

    Refactor project with Lombok

--- src/main/java/guru/springframework/sfgrecipeproject/bootstrap/DataBootstrap.java
+++ src/main/java/guru/springframework/sfgrecipeproject/bootstrap/DataBootstrap.java
@@ -2,9 +2,11 @@ package guru.springframework.sfgrecipeproject.bootstrap;
 
 import guru.springframework.sfgrecipeproject.domain.Recipe;
 import guru.springframework.sfgrecipeproject.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class DataBootstrap implements CommandLineRunner {
     private final RecipeRepository recipeRepository;
@@ -15,15 +17,19 @@ public class DataBootstrap implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        log.debug("DataBootstrap#run start");
         Recipe recipe = new Recipe();
         recipe.setDescription("Perfect Guacamole");
 
-        recipeRepository.save(recipe);
+        Recipe recipeSaved = recipeRepository.save(recipe);
+        log.debug("add recipe:"+recipeSaved.toString());
 
         Recipe recipe2 = new Recipe();
         recipe2.setDescription("Spicy Grilled Chicken Tacos");
 
-        recipeRepository.save(recipe2);
+        Recipe recipe2Saved = recipeRepository.save(recipe2);
+        log.debug("add recipe:"+recipe2Saved.toString());
+        log.debug("DataBootstrap#run end");
 
     }
 }
--- src/main/java/guru/springframework/sfgrecipeproject/controllers/IndexController.java
+++ src/main/java/guru/springframework/sfgrecipeproject/controllers/IndexController.java
@@ -3,10 +3,12 @@ package guru.springframework.sfgrecipeproject.controllers;
 import guru.springframework.sfgrecipeproject.repositories.CategoryRepository;
 import guru.springframework.sfgrecipeproject.repositories.RecipeRepository;
 import guru.springframework.sfgrecipeproject.repositories.UnitOfMeasureRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -23,8 +25,11 @@ public class IndexController {
 
     @RequestMapping("")
     public String index(Model model) {
+        System.out.println(log.isDebugEnabled());
+        log.debug("IndexController#index start");
+        log.debug("find recipes");
         model.addAttribute("recipes", recipeRepository.findAll());
-
+        log.debug("IndexController#index end");
         return "index";
     }
 }
